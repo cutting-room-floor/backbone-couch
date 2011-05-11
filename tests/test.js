877dc4bf67692ca19467ca623be197aeb65c8fc3
Alex Barth <alex@developmentseed.org>
2011-05-11 01:31:11 -0400

    Test for views. Can't run save and view test at the same time.

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,17 @@
 var assert = require('assert'),
     Backbone = require('backbone'),
-    _ = require('underscore');
+    _ = require('underscore')._;
+
+// Global fatal error handler.
+// ---------------------------
+var error = function(db) {
+    return function(result, error) {
+        console.error(new Error(error).stack);
+        db.dbDel(function() {
+            process.exit(0);
+        });
+    }
+};
 
 // Install and destroy database.
 // -----------------------------
@@ -12,7 +23,7 @@ exports['install'] = function() {
         db.get('_design/base', function(err, doc) {
             assert.isNull(err);
             assert.isDefined(doc);
-            couch.db.dbDel(function(err, res) {
+            db.dbDel(function(err, res) {
                 assert.isNull(err);
                 db.get('_design/base', function(err, doc) {
                     assert.eql(err, 'not_found: no_db_file');
@@ -27,45 +38,90 @@ exports['install'] = function() {
 exports['save'] = function() {
     var couch = require('backbone-couch')({name: 'backbone_couch_test_save'});
     var db = couch.db;
-    Number.prototype.sync = couch.sync;
+    TestNumber.prototype.sync = couch.sync;
     couch.install(function(err) {
         var models = [];
         var destroyed = 0;
         var destroy = function(model) {
-            model.destroy(function() {
-                destroyed++;
-                if (destroyed == data.length) {
-                    couch.db.dbDel();
-                }
+            model.destroy({
+                success: function() {
+                    destroyed++;
+                    if (destroyed == data.length) {
+                        db.dbDel();
+                    }
+                },
+                error: error(db)
             });
         };
         var reload = function(model) {
             assert.isDefined(model.get('_rev'));
             var rev = model.get('_rev');
-            (new Number({id: model.id})).fetch({
+            (new TestNumber({id: model.id})).fetch({
                 success: function(model) {
                     assert.eql(model.get('_rev'), rev);
                     destroy(model);
                 },
-                error: function(model) {
-                    console.error('Error fetching model');
-                    destroy(model);
-                }
+                error: error(db)
             });
         };
         _.each(data, function(d) {
-            models.push((new Number()).save(d, {
+            models.push((new TestNumber()).save(d, {
                 success: reload,
-                error: reload
+                error: error(db)
             }));
         });
     });
 };
 
-var Number = Backbone.Model.extend({
+// Use a view to load a collection.
+// --------------------------------
+exports['view'] = function() {
+    var couch = require('backbone-couch')({name: 'backbone_couch_test_view'});
+    var db = couch.db;
+    var ViewNumber = TestNumber.extend({});
+    var ViewNumbers = TestNumbers.extend({
+        model: ViewNumber
+    });
+    TestNumber.prototype.sync = couch.sync;
+    TestNumbers.prototype.sync = couch.sync;
+    couch.install(function(err) {
+        var models = [];
+        var saved = 0;
+        var loadAll = function() {
+            saved++;
+            if (saved == data.length) {
+                (new ViewNumbers()).fetch({
+                    success: function(collection) {
+                        var names = collection.map(function(model) {
+                            return model.get('name');
+                        });
+                        _.each(data, function(d) {
+                            assert.eql(true, names.indexOf(d.name) != -1);
+                        });
+                        db.dbDel();
+                    },
+                    error: error(db)
+                })
+            }
+        };
+        _.each(data, function(d) {
+            models.push((new ViewNumber()).save(d, {
+                success: loadAll,
+                error: error(db)
+            }));
+        });
+    });
+};
+
+var TestNumber = Backbone.Model.extend({
     url: function() {
         return '/api/Number/' + this.id;
     }
+});
+
+var TestNumbers = Backbone.Collection.extend({
+    model: TestNumber,
+    url: '/api/Number'
 });
 
 var data = [{
